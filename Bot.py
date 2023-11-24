42aaa00ae8ba91d3966953c3d48762ce85b12486
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-11-25 03:53:14 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,40 +1,23 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-
-
+import telebot
+from telebot import types
+import os
 
 TOKEN = os.getenv('TELEGRAM_BOT_API_ID')
+bot = telebot.TeleBot(TOKEN)
 
-def start(update, context):
-    keyboard = [
-    [InlineKeyboardButton("🌐︎ Wᴇʙsɪᴛᴇ", url="https://jinix6.github.io/Craftland"),
-     InlineKeyboardButton("📷 Iɴsᴛᴀɢʀᴀᴍ", url="https://instagram.com/jinxx6_6?igshid=MzNlNGNkZWQ4Mg=="),
-    InlineKeyboardButton("😺 GɪᴛHᴜʙ", url="https://github.com/jinix6")],
-     [InlineKeyboardButton("🚀 Tᴇʟᴇɢʀᴀᴍ", url="https://t.me/Jinxx6_6_real"),
-     InlineKeyboardButton("🚀 Tᴇʟᴇɢʀᴀᴍ 2", url="https://t.me/crystal_v1")]
-]
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.row(types.InlineKeyboardButton("🌐︎ Wᴇʙsɪᴛᴇ", url="https://jinix6.github.io/Craftland"),
+                types.InlineKeyboardButton("📷 Iɴsᴛᴀɢʀᴀᴍ", url="https://instagram.com/jinxx6_6?igshid=MzNlNGNkZWQ4Mg=="),
+                types.InlineKeyboardButton("😺 GɪᴛHᴜʙ", url="https://github.com/jinix6"))
+    keyboard.row(types.InlineKeyboardButton("🚀 Tᴇʟᴇɢʀᴀᴍ", url="https://t.me/Jinxx6_6_real"),
+                types.InlineKeyboardButton("🚀 Tᴇʟᴇɢʀᴀᴍ 2", url="https://t.me/crystal_v1"))
 
+    bot.send_message(message.chat.id, '🔗 Hᴇʀᴇ Aʀᴇ Sᴏᴍᴇ ʟɪɴᴋs:', reply_markup=keyboard)
 
-    reply_markup = InlineKeyboardMarkup(keyboard, resize_keyboard=True)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_unknown(message):
+    bot.send_message(message.chat.id, "Uɴᴋɴᴏᴡɴ Mᴇssᴀɢᴇ. Pʟᴇᴀsᴇ Usᴇ Tʜᴇ /start Cᴏᴍᴍᴀɴᴅ.")
 
-    update.message.reply_text('🔗 Hᴇʀᴇ Aʀᴇ Sᴏᴍᴇ ʟɪɴᴋs:', reply_markup=reply_markup)
-
-def handle_unknown(update, context):
-
-    update.message.reply_text("Uɴᴋɴᴏᴡɴ Mᴇssᴀɢᴇ. Pʟᴇᴀsᴇ Usᴇ Tʜᴇ /start Cᴏᴍᴍᴀɴᴅ.")
-
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    
-
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_unknown))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.polling()
